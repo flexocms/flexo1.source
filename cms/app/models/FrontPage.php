2fcdf0513891b09d0cfacaa6954ebc4d7c8282f8
Ivan <gizomo@ya.ru>
2013-07-26 22:57:25 +0700

    Update FrontPage.php

--- cms/app/models/FrontPage.php
+++ cms/app/models/FrontPage.php
@@ -645,38 +645,5 @@ class FrontPage
         // Run!
         while ($object = $stmt->fetchObject())
              $this->tags[$object->id] = $object->tag;
-    }
-    
-    public function tagger_url($page_slug = NULL)
-    {
-        $page_option = ($page_slug !== NULL) ? " AND slug = {$page_slug}" : "";
-        $sql = 'SELECT id FROM '.TABLE_PREFIX.'page WHERE behavior_id = "tagger"'.$page_option;
-        $stmt = Record::getConnection()->prepare($sql);
-        $stmt->execute();
-        $id = $stmt->fetchColumn();
-        if(!is_null($id) && $id !== false)
-        {
-           $page = Page::findbyId($id);
-           $url = $page->getUri();
-           return BASE_URL.$url.'/';
-        }
-        else
-        {
-           return self::tagger_url(NULL);
-        }
-    }
-
-    public function tags_list($tags, $option = array())
-    {
-       $separator = array_key_exists('separator', $option) ? $option['separator'] : ', ';
-       $tagger_page_slug = array_key_exists('tagger_page_slug', $option) ? $option['tagger_page_slug'] : NULL;
-       $i = 1;
-       foreach($tags as $tag)
-       {
-          $url = self::tagger_url($tagger_page_slug).$tag.URL_SUFFIX;
-          $end = $i == count($tags) ? '.' : $separator;
-          echo sprintf('<a href="%s">%s</a>%s', $url, $tag, $end);
-          $i++;
-       }
     }	
 } // end FrontPage class
